89acd159b38fceba1fbce76d72803e25fbb228c1
byggis <byggis@hibox.fi>
2017-02-21 18:57:13 +0200

    Added some methods.

--- src/database.cpp
+++ src/database.cpp
@@ -1,3 +1,4 @@
+#include <QtSql>
 #include "database.h"
 
 /**
@@ -6,18 +7,25 @@
  */
 
 void Database::close() {
-
+	db.close();
+	db.removeDatabase( QSqlDatabase::defaultConnection );
 }
 
 bool Database::open() {
-
+	if ( !db.open() ) {
+		qDebug() << "Unable to open database: " << db.databaseName();
+		return false;
+	}
+	qDebug() << "db was successfully opened";
+	return true;
 }
 
 Database::~Database() {
-
+	close();
 }
 
 Database::Database() {
-
+	db = QSqlDatabase::addDatabase( "QSQLITE" );
+	db.setDatabaseName( "magnifising.sqlite" );
 }
 
