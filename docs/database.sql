5520b56033e70b108365fdb70fcceea9a048e89f
Fredrik <fredrik.byggmastar@gmail.com>
2017-03-01 23:23:39 +0200

    Added files and performances tables to database.

--- docs/database.sql
+++ docs/database.sql
@@ -1,9 +1,22 @@
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
   id integer primary key,
   name text unique,
   realname text
 );
-
 INSERT INTO users ( name, realname ) values ( 'klaus', 'Klaus Lapela' );
 
+CREATE TABLE files (
+  id integer primary key,
+  filename text unique
+);
+
+CREATE TABLE performances (
+  userid integer,
+  fileid integer,
+  time datetime,
+  quality double,
+  FOREIGN KEY( userid ) REFERENCES users( id ),
+  FOREIGN KEY( fileid ) REFERENCeS files( id )
+);
